0313a1102bc88b55e4cc3291693938987f33f72a
Varoot Phasuthadol <varoot.phasuthadol@accenture.com>
2020-01-13 14:55:33 +0700

    AppBar

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <CssBaseline>
+    <App />
+  </CssBaseline>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
